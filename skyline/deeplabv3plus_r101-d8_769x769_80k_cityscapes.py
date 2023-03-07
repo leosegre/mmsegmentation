0f5eb037e66e9f966d98aaf4e0442e2d8b5f0623
leosegre <58905206+leosegre@users.noreply.github.com>
2023-03-07 14:19:11 +0200

    custom dataset multi gpu

--- skyline/deeplabv3plus_r101-d8_769x769_80k_cityscapes.py
+++ skyline/deeplabv3plus_r101-d8_769x769_80k_cityscapes.py
@@ -156,7 +156,7 @@ data = dict(
 log_config = dict(
     interval=50, hooks=[dict(type='TextLoggerHook', by_epoch=False)])
 dist_params = dict(backend='nccl')
-mp_start_method = 'fork'
+mp_start_method = 'spawn'
 log_level = 'INFO'
 load_from = None
 resume_from = None
