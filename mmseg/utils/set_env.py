6f7259c0d57519f6ef0d6e04c494f105ff4e10bf
leosegre <58905206+leosegre@users.noreply.github.com>
2023-03-07 17:00:36 +0200

    multi gpu

--- mmseg/utils/set_env.py
+++ mmseg/utils/set_env.py
@@ -8,9 +8,9 @@ import torch.multiprocessing as mp
 from ..utils import get_root_logger
 
 
-def setup_multi_processes(cfg, logger):
+def setup_multi_processes(cfg):
     """Setup multi-processing environment variables."""
-    # logger = get_root_logger()
+    logger = get_root_logger()
 
     # set multi-process start method
     if platform.system() != 'Windows':
