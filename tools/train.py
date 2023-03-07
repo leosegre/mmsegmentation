70ce3f7d543ed33daa05435cba07762c74ce2575
leosegre <58905206+leosegre@users.noreply.github.com>
2023-03-07 17:07:34 +0200

    multi gpu

--- tools/train.py
+++ tools/train.py
@@ -167,7 +167,7 @@ def main():
     logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
 
     # set multi-process settings
-    setup_multi_processes(cfg)
+    setup_multi_processes(cfg, logger)
 
     # init the meta dict to record some important information such as
     # environment info and seed, which will be logged
@@ -190,7 +190,7 @@ def main():
     seed = seed + dist.get_rank() if args.diff_seed else seed
     logger.info(f'Set random seed to {seed}, '
                 f'deterministic: {args.deterministic}')
-    logger.error(f'os.environ["LOCAL_RANK"]: {os.environ["LOCAL_RANK"]}')
+    logger.info(f'os.environ["LOCAL_RANK"]: {os.environ["LOCAL_RANK"]}')
     set_random_seed(seed, deterministic=args.deterministic)
     cfg.seed = seed
     meta['seed'] = seed
