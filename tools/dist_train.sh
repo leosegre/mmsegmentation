8600dfeb043cafe68ce30463c2952b1c870623f2
leosegre <58905206+leosegre@users.noreply.github.com>
2023-03-07 14:58:16 +0200

    multi gpu

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -16,6 +16,13 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 #    $CONFIG \
 #    --launcher pytorch ${@:3}
 
-torchrun $(dirname "$0")/train.py \
-#    $CONFIG \
-#    --launcher pytorch ${@:3}
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+torchrun \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    $(dirname "$0")/train.py \
+    $CONFIG \
+    --launcher pytorch ${@:3}
