2e32be63eabf1132a8d7f6e150522da2fcda7c03
leo <leosegre@gmail.com>
2023-05-21 14:47:03 +0300

    ignore 0

--- tools/model_ensemble.py
+++ tools/model_ensemble.py
@@ -80,6 +80,7 @@ def main(args):
         for logit in result:
             result_logits += logit
 
+        print(result_logits.shape)
         pred_to_show = result_logits.argmax(axis=1)
         pred = pred_to_show.squeeze()
         img_info = dataset.img_infos[batch_indices[0]]
