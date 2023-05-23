5a6cf7fd9aa7816b3ab1e17110cd5aa400b24b40
leo <leosegre@gmail.com>
2023-05-23 09:42:25 +0300

    classes

--- mmseg/datasets/custom.py
+++ mmseg/datasets/custom.py
@@ -76,7 +76,11 @@ class CustomDataset(Dataset):
     CLASSES = ("unclass", "road", "sidewalk", "water", "car", "tree",
                "building", "door", "roof", "window")
 
-
+    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+               [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
+               [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
+               [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
+               [0, 80, 100], [0, 0, 230], [119, 11, 32]]
 
     def __init__(self,
                  pipeline,
@@ -109,6 +113,7 @@ class CustomDataset(Dataset):
                    [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
                    [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
                    [0, 80, 100], [0, 0, 230], [119, 11, 32]]
+
         self.CLASSES, self.PALETTE = self.get_classes_and_palette(
             classes, palette)
         self.gt_seg_map_loader = LoadAnnotations(
