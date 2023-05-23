49880694c0f15a6e938681a6178e9991050b0e70
leo <leosegre@gmail.com>
2023-05-23 09:41:02 +0300

    classes

--- mmseg/datasets/custom.py
+++ mmseg/datasets/custom.py
@@ -104,13 +104,13 @@ class CustomDataset(Dataset):
         self.ignore_index = ignore_index
         self.reduce_zero_label = reduce_zero_label
         self.label_map = None
-        PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+        palette = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
                    [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
                    [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
                    [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
                    [0, 80, 100], [0, 0, 230], [119, 11, 32]]
         self.CLASSES, self.PALETTE = self.get_classes_and_palette(
-            classes, PALETTE)
+            classes, palette)
         self.gt_seg_map_loader = LoadAnnotations(
             reduce_zero_label=reduce_zero_label, **gt_seg_map_loader_cfg)
 
